38c31404e4fdc3eb2dc32346ae6e30da8e56ee0c
lorenzosinisi <lasslo.net@gmail.com>
2019-11-06 17:18:10 +0100

    Add mix meta for hex.pm

--- mix.exs
+++ mix.exs
@@ -1,27 +1,46 @@
 defmodule Duration.MixProject do
   use Mix.Project
 
+  @version "0.1.1"
   def project do
     [
       app: :duration,
-      version: "0.1.0",
-      elixir: "~> 1.8",
-      start_permanent: Mix.env() == :prod,
+      version: @version,
+      elixir: "~> 1.9",
+      docs: [extras: ["README.md"], main: "readme", source_ref: "v#{@version}"],
+      source_url: "https://github.com/lorenzosinisi/duration",
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:timex, "~> 3.5"}
+      {:timex, "~> 3.5"},
+      {:ex_doc, "~> 0.16", only: :dev},
+      {:earmark, "~> 1.2", only: :dev}
+    ]
+  end
+
+  defp description do
+    """
+    A wrapper around :timer.tc in Elixir to know how long it takes to execute a function
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Lorenzo Sinisi"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/lorenzosinisi/duration"}
     ]
   end
 end
